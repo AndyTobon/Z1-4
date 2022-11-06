0598418533aeb9f967ae6c6a6f55b0f95c6266fa
AndyTobon <111801815+AndyTobon@users.noreply.github.com>
2022-11-06 15:06:16 -0500

    nuevo

--- app/src/main/java/com/example/myapplication/Catgatos.java
+++ app/src/main/java/com/example/myapplication/Catgatos.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CatePerros extends AppCompatActivity {
+public class Catgatos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cate_perros);
+        setContentView(R.layout.activity_catgatos);
     }
 }
